3b88eb3bc48a560951aa009754ec4a22fc27fa9e
poorani <gnanapoorani@users.noreply.github.com>
2024-03-27 10:58:49 +0530

    Strings

---
+++ RemoveOutermostParanthesis.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class RemoveOutermostParanthesis {
+    public static void main(String[] args) {
+        System.out.println(removeOuterParentheses("(()())(())"));
+    }
+
+    public static String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stringStack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                stringStack.pop();
+                if (stringStack.size() > 0) {
+                    result.append(c);
+                }
+
+            } else {
+                if (stringStack.size() > 0) {
+                    result.append(c);
+                }
+                stringStack.push(c);
+            }
+        }
+        return result.toString();
+    }
+
+}
